ba35e157b51a840d9e3899c99e0ae2412e6e7cb2
sufyan <sufyan@logicalcommerce.com>
2022-11-19 22:49:20 +0500

    Done

--- app/head.jsx
+++ app/head.jsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { getPageData } from "./api";
 
 export default async function Head() {
@@ -9,6 +10,20 @@ export default async function Head() {
       <meta name="description" content={data.header.meta.description} />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      {data.header.schemas.map((item, i) => {
+        return Object.entries(item).map(([className, schema], j) => {
+          if (!schema) return <></>;
+          return (
+            <script
+              async=""
+              key={i + j}
+              type="application/ld+json"
+              className={className}
+              dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+            />
+          );
+        });
+      })}
     </>
   );
 }
