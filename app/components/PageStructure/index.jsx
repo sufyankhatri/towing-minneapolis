4f3125b2f552b306f9ebc3a09b7dcce0a332a635
Usama Bhatti <70816277+usamamustafa778@users.noreply.github.com>
2022-11-21 19:58:01 -0800

    Conflicts REsolved

--- app/components/PageStructure/index.jsx
+++ app/components/PageStructure/index.jsx
@@ -32,17 +32,15 @@ const PageStructure = ({ data, params }) => {
         <Services data={data.services} contact={data.contact} />
         <AllServices data={data.allServices} params={params} />
         <Reviews data={data.reviews} />
-        <CallBack data={data.form} />
         <Help data={data.help} contact={data.contact} />
+        <CallBack data={data.form} />
       </main>
-      <footer>
-        <Footer
-          data={data.footer}
-          contact={data.contact}
-          brand={data.header.navbar.brand}
-          params={params}
-        />
-      </footer>
+      <Footer
+        data={data.footer}
+        contact={data.contact}
+        brand={data.header.navbar.brand}
+        params={params}
+      />
       {/* {data.header.schemas.map((item, i) =>
         Object.entries(item).map(([className, schema], j) => (
           <script key={i + j} type="application/ld+json" className={className}>
